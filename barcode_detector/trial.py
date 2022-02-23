a29ace927ed26cd8d2851879debc06ed9467f517
Laura Arias Fernandez <larfer2001@gmail.com>
2022-02-23 08:39:08 -0600

    finalize location of barcode

--- barcode_detector/trial.py
+++ barcode_detector/trial.py
@@ -1,12 +1,14 @@
 
 # Run the file that detects the barcode 
+import re
 import subprocess
 import shutil
 
 import cv2
 import time
 from cv2 import imshow
-from cv2 import waitKey 
+from cv2 import waitKey
+from prettytable import NONE 
 results_path = "../yolov5-master/runs/detect/barcode/labels/results.txt"
 timeout_video = 60
 
@@ -20,17 +22,19 @@ def barcode_obj_detect(path):
 
     subprocess.run(['python3', '../yolov5-master/detect.py',"--weights", "barcode_model.pt", "--source" ,path,"--name","barcode","--save-txt"])
 
-    f = open(results_path, "r")
-    # get txt value
-    for line in f:
-        if line[0] =="1":
-            print("barcode detected")
-            result = line.split(" ")
-            result = result[1: ]
-            for i in range(len(result)):
-                result[i] = float(result[i])
-    f.close()
-    
+    result = None
+    if (exists(results_path)):
+        f = open(results_path, "r")
+        # get txt value
+        for line in f:
+            if line[0] =="1":
+                print("barcode detected")
+                result = line.split(" ")
+                result = result[1: ]
+                for i in range(len(result)):
+                    result[i] = float(result[i])
+        f.close()
+        
     return result # [center_x, center_y, w,h]
 
 
@@ -44,8 +48,8 @@ def video_capture():
     capture = cv2.VideoCapture(0)
 
     #size of the screen shown 
-    capture.set(3,640)#width
-    capture.set(4,480)#height
+    capture.set(3,416)#width
+    capture.set(3,416)#height
 
     ## start time out for camera
     startTime =  time.time()
@@ -67,24 +71,32 @@ def video_capture():
                 return barcodes
             else:
                 if (timeElapsed%30==0):
-                    #every 30 second run obj detector to check if barcode is present and can could not be scannerd
+                    #every 30 second run obj detector to check if barcode is present 
                     #check barcode if exists cut it and pass it to check barcodes
-                    cv2.imwrite("../yolov5-master/runs/detect//img.jpg", img)  # 480 by 640
-                    res = barcode_obj_detect("../yolov5-master/runs/detect//img.jpg")
+                    #resize img
+                    resized = cv2.resize(img,(416,416),interpolation=cv2.INTER_AREA)
+                    cv2.imwrite("../yolov5-master/runs/detect/img.jpg", resized) 
+                    res = barcode_obj_detect("../yolov5-master/runs/detect/img.jpg")
 
-                    if res !=[]:
-                        center_x,center_y,w,h = res
-                        x,y,_ = img.shape
-                        
-                        width = int (x * w)
-                        height = int (h*y)
-                        cent_x = int(center_x*x) 
-                        cent_y = int(center_y*y) 
-                        x_Start = cent_x - width//2
-                        y_Start = cent_y - height//2
+                    if res !=None:
+                        xcenter, ycenter, b_width,b_height  = res
 
-                        cropped_image = img[x_Start:, y_Start:]
-                        imshow("img",cropped_image)
+                        width,height,_ = resized.shape
+                        center_x = int (width* xcenter)
+                        center_y = int(height *ycenter)
+                        print ("the center is ",center_x," ", center_y)
+                        print ("the shap is ",img.shape)
+
+                        obj_height  = int(height * b_height)
+                        obj_width = int(width * b_width)
+
+                        ymin = center_y - obj_height
+                        xmin = center_x - obj_width
+                        xmax = center_x + obj_width
+                        ymax = center_y + obj_height
+
+                        cv2.rectangle(resized, (xmin, ymin), (xmax, ymax), (0, 255, 0))
+                        imshow("img",resized)
                         waitKey(0)
 
                 
